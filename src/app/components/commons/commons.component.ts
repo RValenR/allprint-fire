1f6a1ad309e213e9abf795d328f7b4ef1e00eb35
Alejo <rafael.valencia.r4@gmail.com>
2024-12-19 13:33:23 -0500

    estructura incial

---
+++ src/app/components/commons/commons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commons',
+  standalone: true,
+  imports: [],
+  templateUrl: './commons.component.html',
+  styleUrl: './commons.component.css'
+})
+export class CommonsComponent {
+
+}
---
+++ src/app/components/our-info/our-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-info.component.html',
+  styleUrl: './our-info.component.css'
+})
+export class OurInfoComponent {
+
+}
