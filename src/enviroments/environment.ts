3f701f2bdf29f691ff4a34b465948481ffb30ed8
Exsanty96 <santiremo10@gmail.com>
2025-01-17 17:06:16 -0500

    Instalación angular

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,13 +1,10 @@
-export const environment = {
-    production: false,
-    firebase: {
-      apiKey: "TU_API_KEY",
-      authDomain: "TU_AUTH_DOMAIN",
-      databaseURL: "TU_DATABASE_URL",
-      projectId: "TU_PROJECT_ID",
-      storageBucket: "TU_STORAGE_BUCKET",
-      messagingSenderId: "TU_MESSAGING_SENDER_ID",
-      appId: "TU_APP_ID",
-    },
-  };
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPudU8TSe3Vc9pEgMXJ6qpO8dVREQAcNk",
+  authDomain: "allprint-c12c5.firebaseapp.com",
+  projectId: "allprint-c12c5",
+  storageBucket: "allprint-c12c5.firebasestorage.app",
+  messagingSenderId: "699278202294",
+  appId: "1:699278202294:web:365bba26c151ce21e2dd17",
+  measurementId: "G-KQ2R0FMZS0"
+};
   
