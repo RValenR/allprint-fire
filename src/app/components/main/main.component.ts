3a390dff14935ac0c3982ca5a1ec9a3618469041
alejo <rafael.valencia.r4@gmail.com>
2025-03-16 00:23:17 -0500

    restructuracion landing principal

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,9 +3,12 @@ import { GalleriaModule } from 'primeng/galleria';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { RouterModule,Router } from '@angular/router';
 import Swiper from 'swiper';
+
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { DataService } from '../../services/data.service';
 import { HttpClientModule } from '@angular/common/http';  // Importa el módulo HttpClient
+import { BannerImage } from '../../interfaces/banner-image.interface';  // Importar la interfaz
+
 @Component({
   selector: 'app-main',
   standalone: true,
@@ -15,25 +18,67 @@ import { HttpClientModule } from '@angular/common/http';  // Importa el módulo 
 })
 export class MainComponent implements OnInit, AfterViewInit{
   @ViewChild('swiperRef', { static: false }) swiperRef!: ElementRef;
-  swiper!: Swiper;
 
-  images: string[] = [
-    'assets/images/fondo3.png',
-    'assets/images/fondo1.png',
-    'assets/images/fondo2.png' 
+  @ViewChild('swiperRef2', { static: false }) swiperRef2!: ElementRef;
+
+  swiper!: Swiper;
+  swiper2!: Swiper;
+
+  images: string[] =[];
+  datos =[];
+
+  clienteLogos: string[] = [
+    'assets/images/logos/bancosolidario.png',
+    'assets/images/logos/edimca.png',
+    'assets/images/logos/grunental.png',
+    'assets/images/logos/biess.png',
+    'assets/images/logos/humana.webp',
+    'assets/images/logos/santillana.png',
+    'assets/images/logos/fybeca.png',
+    'assets/images/logos/sri.png',
+    'assets/images/logos/kia.png',
+    'assets/images/logos/procredit.png',
+    'assets/images/logos/kfc.png',
+    'assets/images/logos/sukasa.png',
+    'assets/images/logos/gm.png',
+    'assets/images/logos/giz.png',
+    'assets/images/logos/meditropoli.png',
+    'assets/images/logos/citimed.png',
+    'assets/images/logos/toyota.png',
+    'assets/images/logos/xerox.png'
   ];
-  
+
+  swiperConfig = {
+    slidesPerView: 4, // Mostrar 4 imágenes a la vez
+    spaceBetween: 20, // Espaciado entre imágenes
+    loop: true, // Para que el carrusel sea infinito
+    navigation: true, // Habilitar navegación
+    autoplay: {
+      delay: 3000, // Cambia cada 3 segundos
+      disableOnInteraction: false
+    }
+  };
 
   constructor(private router: Router, 
     private dataService: DataService
   ) {}
-  datos =[]
+
   ngOnInit() {
     this.dataService.getDatos().subscribe(
       (data) => {
         this.datos = data;
         console.log(data)
 
+      },
+      (error) => {
+        console.error('Error al obtener datos', error);
+      }
+    );
+
+    this.dataService.getBannerImages().subscribe(
+      (response: BannerImage[]) => {
+        // Extraer solo las URLs de la respuesta
+        this.images = response.map(item => item.url);
       },
       (error) => {
         console.error('Error al obtener datos', error);
@@ -53,6 +98,20 @@ export class MainComponent implements OnInit, AfterViewInit{
 
   ngAfterViewInit() {
     this.swiper = new Swiper(this.swiperRef.nativeElement, {
+      modules: [Navigation, Pagination, Autoplay],
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Permite hacer clic en los puntos de la paginación
+      },
+      autoplay: { delay: 3000, disableOnInteraction: false },
+    });
+
+    this.swiper2 = new Swiper(this.swiperRef2.nativeElement, {
       modules: [Navigation, Pagination, Autoplay],
       loop: true,
       navigation: {
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { BannerImage } from '../interfaces/banner-image.interface';  // Importar la interfaz
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:3000/api/datos';  // Cambia esta URL a la de tu API
-
+  private apiUrl = 'https://allprint-backend.vercel.app/api/datos';  // Cambia esta URL a la de tu API
+  private imgApi = 'http://localhost:3000/imagenes/banner';
   constructor(private http: HttpClient) {}
 
   getDatos(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
+  }
+
+  getBannerImages(): Observable<BannerImage[]> {
+    return this.http.get<BannerImage[]>(this.imgApi);
   }
 }
