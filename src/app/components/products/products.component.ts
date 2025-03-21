f1668d2ccd812edb7ec5cb0d60859375626dec7d
Exsanty96 <santiremo10@gmail.com>
2025-03-21 18:31:35 -0500

    Aside con Conexion a la BDD Categoria

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import { CategoriasService } from '../../services/categorias.service';
+import { CommonModule } from '@angular/common';   
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {  // Cambiar a ProductsComponent
+  categorias: any[] = [];
 
+  constructor(private categoriasService: CategoriasService) {}
+
+  ngOnInit(): void {
+    this.categoriasService.getCategorias().subscribe((data) => {
+      this.categorias = data;
+    });
+  }
 }
