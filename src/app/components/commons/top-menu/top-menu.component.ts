49d5561b1f46313bef701338cbe1692bad6ed7a3
Exsanty96 <santiremo10@gmail.com>
2025-04-03 18:38:40 -0500

    Boton Ubicanos

--- src/app/components/commons/top-menu/top-menu.component.ts
+++ src/app/components/commons/top-menu/top-menu.component.ts
@@ -1,26 +1,49 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ParametrosService } from '../../../services/parametros.service';
+import { CommonModule } from '@angular/common'; // Necesario para *ngIf
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule], // Añade CommonModule
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.css'
 })
-export class TopMenuComponent {
+export class TopMenuComponent implements OnInit {
   @Output() actionSelected: EventEmitter<any> = new EventEmitter();
-
   menuOpen = false;
+  mapsUrl: string = ''; // Variable para almacenar la URL de Maps
+
+  constructor(private parametrosService: ParametrosService) {}
+
+  ngOnInit() {
+    this.loadMapsUrl();
+  }
+
+  async loadMapsUrl() {
+    try {
+      const parametros = await this.parametrosService.getParametros().toPromise();
+      this.mapsUrl = parametros.maps_ubicacion;
+    } catch (error) {
+      console.error('Error al cargar parámetros:', error);
+    }
+  }
 
   addNewData() {
-    this.actionSelected.emit({ action: 'addNew' })
+    this.actionSelected.emit({ action: 'addNew' });
   }
-  logOut(){
-    this.actionSelected.emit({ action: 'logOut' })
+
+  logOut() {
+    this.actionSelected.emit({ action: 'logOut' });
   }
 
   toggleMenu() {
-    this.menuOpen = !this.menuOpen; // Alterna el estado del menú
+    this.menuOpen = !this.menuOpen;
+  }
+  openMaps() {
+    if (this.mapsUrl) {
+      window.open(this.mapsUrl, '_blank');
+    }
   }
 }
