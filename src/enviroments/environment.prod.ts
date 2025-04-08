fa0be0e62e4845a9bea86db188a2a1158fb6efec
Exsanty96 <santiremo10@gmail.com>
2025-04-08 10:34:37 -0500

    Variables de entorno

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://allprint-backend.vercel.app'
+};
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPudU8TSe3Vc9pEgMXJ6qpO8dVREQAcNk",
+  authDomain: "allprint-c12c5.firebaseapp.com",
+  projectId: "allprint-c12c5",
+  storageBucket: "allprint-c12c5.firebasestorage.app",
+  messagingSenderId: "699278202294",
+  appId: "1:699278202294:web:365bba26c151ce21e2dd17",
+  measurementId: "G-KQ2R0FMZS0"
+};
+
